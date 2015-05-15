775a66344b562b55335a11e5e7378c411150485d
kgryte <kgryte@gmail.com>
2015-05-14 17:39:17 -0700

    [UPDATE] dotfiles. Makefile. copy and accessor options. validate deps. [TESTS] input args. code pathways.

--- README.md
+++ README.md
@@ -2,7 +2,7 @@ Divide
 ===
 [![NPM version][npm-image]][npm-url] [![Build Status][travis-image]][travis-url] [![Coverage Status][coveralls-image]][coveralls-url] [![Dependencies][dependencies-image]][dependencies-url]
 
-> Computes an element-wise division of a numeric array.
+> Computes an element-wise division.
 
 
 ## Installation
@@ -16,23 +16,102 @@ For use in the browser, use [browserify](https://github.com/substack/node-browse
 
 ## Usage
 
-To use the module,
-
 ``` javascript
 var divide = require( 'compute-divide' );
 ```
 
-#### divide( arr, x )
+#### divide( arr, x[, opts] )
 
-Computes an element-wise division of an input `array`. `x` may be either an `array` of equal length or a scalar.
+Computes an element-wise division. `x` may be either an `array` of equal length or a `numeric` value.
 
 ``` javascript
-divide( [ 2, 1, 4, 2 ], 2 );
+var arr = [ 2, 1, 4, 2 ],
+	out;
+
+out = divide( arr, 2 );
 // returns [ 1, 0.5, 2, 1 ]
 
-divide( [ 2, 1, 3, -6 ], [ 1, 2, 4, 3 ] );
-// returns [ 2, 0.5, 0.75, -2 ]
+out = divide( arr, [ 1, 2, 8, 8 ] );
+// returns [ 2, 1, 0.5, 0.25 ]
 ```
+
+The function accepts the following `options`:
+
+*  __copy__: `boolean` indicating whether to return a new `array`. Default: `true`.
+*  __accessor__: accessor `function` for accessing values in object `arrays`.
+
+To mutate the input `array` (e.g., when input values can be discarded or when optimizing memory usage), set the `copy` option to `false`.
+
+``` javascript
+var arr = [ 5, 3, 8, 3, 2 ];
+
+var out = divide( arr, 4, {
+	'copy': false
+});
+// returns [ 1.25, 0.75, 0.5, 0.75, 0.5 ]
+
+console.log( arr === out );
+// returns true
+```
+
+__Note__: mutation is the `array` equivalent of a __slash-equal__ (`/=`).
+
+For object `arrays`, provide an accessor `function` for accessing `array` values.
+
+``` javascript
+var data = [
+	['beep', 5],
+	['boop', 3],
+	['bip', 8],
+	['bap', 3],
+	['baz', 2]
+];
+
+function getValue( d, i ) {
+	return d[ 1 ];
+}
+
+var out = divide( data, 4, {
+	'accessor': getValue
+});
+// returns [ 1.25, 0.75, 2, 0.75, 0.5 ]
+```
+
+When dividing values between two object `arrays`, provide an accessor `function` which accepts `3` arguments.
+
+``` javascript
+var data = [
+	['beep', 5],
+	['boop', 3],
+	['bip', 8],
+	['bap', 3],
+	['baz', 2]
+];
+
+var arr = [
+	{'x': 4},
+	{'x': 5},
+	{'x': 6},
+	{'x': 5},
+	{'x': 3}
+];
+
+function getValue( d, i, j ) {
+	if ( j === 0 ) {
+		return d[ 1 ];
+	}
+	return d.x;
+}
+
+var out = divide( data, arr, {
+	'accessor': getValue
+});
+// returns [ 1.25, 0.6, ~1.33, 0.6, ~0.67 ]
+```
+
+__Note__: `j` corresponds to the input `array` index, where `j=0` is the index for the first input `array` and `j=1` is the index for the second input `array`.
+
+
 
 
 ## Examples
@@ -40,16 +119,14 @@ divide( [ 2, 1, 3, -6 ], [ 1, 2, 4, 3 ] );
 ``` javascript
 var divide = require( 'compute-divide' );
 
-// Simulate some data...
 var data = new Array( 100 );
-
 for ( var i = 0; i < data.length; i++ ) {
 	data[ i ] = Math.round( Math.random()*100 );
 }
 
-divide( data, 10 );
+var out = divide( data, 10 );
 
-console.log( data.join( '\n' ) );
+console.log( out.join( '\n' ) );
 ```
 
 To run the example code from the top-level application directory,
@@ -59,23 +136,11 @@ $ node ./examples/index.js
 ```
 
 
-## Notes
-
-This function mutates the input `array`. If mutation is undesired,
-
-``` javascript
-var data = [ 1, 2, 3, 4 ],
-	copy = data.slice();
-
-divide( copy, 2 );
-```
-
-
 ## Tests
 
 ### Unit
 
-Unit tests use the [Mocha](http://visionmedia.github.io/mocha) test framework with [Chai](http://chaijs.com) assertions. To run the tests, execute the following command in the top-level application directory:
+Unit tests use the [Mocha](http://mochajs.org) test framework with [Chai](http://chaijs.com) assertions. To run the tests, execute the following command in the top-level application directory:
 
 ``` bash
 $ make test
@@ -99,15 +164,15 @@ $ make view-cov
 ```
 
 
+---
 ## License
 
 [MIT license](http://opensource.org/licenses/MIT). 
 
 
----
 ## Copyright
 
-Copyright &copy; 2014. Athan Reines.
+Copyright &copy; 2015. The Compute.io Authors.
 
 
 [npm-image]: http://img.shields.io/npm/v/compute-divide.svg
--- test/test.js
+++ test/test.js
@@ -1,3 +1,4 @@
+/* global require, describe, it */
 'use strict';
 
 // MODULES //
@@ -23,17 +24,17 @@ describe( 'compute-divide', function tests() {
 		expect( divide ).to.be.a( 'function' );
 	});
 
-	it( 'should throw an error if provided a non-array', function test() {
+	it( 'should throw an error if not provided an array', function test() {
 		var values = [
-				5,
-				'5',
-				{},
-				true,
-				null,
-				undefined,
-				NaN,
-				function(){}
-			];
+			'5',
+			5,
+			null,
+			undefined,
+			NaN,
+			true,
+			{},
+			function(){}
+		];
 
 		for ( var i = 0; i < values.length; i++ ) {
 			expect( badValue( values[i] ) ).to.throw( TypeError );
@@ -41,21 +42,22 @@ describe( 'compute-divide', function tests() {
 
 		function badValue( value ) {
 			return function() {
-				divide( value, 0 );
+				divide( value, 10 );
 			};
 		}
 	});
 
-	it( 'should throw an error if not provided an array or number for the second argument', function test() {
+	it( 'should throw an error if provided a second argument which is not an array or number primitive', function test() {
 		var values = [
-				'5',
-				{},
-				true,
-				null,
-				undefined,
-				NaN,
-				function(){}
-			];
+			new Number( 5 ),
+			'5',
+			null,
+			undefined,
+			NaN,
+			true,
+			{},
+			function(){}
+		];
 
 		for ( var i = 0; i < values.length; i++ ) {
 			expect( badValue( values[i] ) ).to.throw( TypeError );
@@ -63,32 +65,202 @@ describe( 'compute-divide', function tests() {
 
 		function badValue( value ) {
 			return function() {
-				divide( [], value );
+				divide( [1,2,3], value );
 			};
 		}
 	});
 
-	it( 'should throw an error if provided arrays of unequal length', function test() {
+	it( 'should throw an error if provided an options argument which is not an object', function test() {
+		var values = [
+			'5',
+			5,
+			null,
+			undefined,
+			NaN,
+			true,
+			[],
+			function(){}
+		];
+
+		for ( var i = 0; i < values.length; i++ ) {
+			expect( badValue( values[i] ) ).to.throw( TypeError );
+		}
+
+		function badValue( value ) {
+			return function() {
+				divide( [1,2,3], 10, value );
+			};
+		}
+	});
+
+	it( 'should throw an error if provided a copy option which is not a boolean primitive', function test() {
+		var values = [
+			'5',
+			5,
+			null,
+			undefined,
+			NaN,
+			new Boolean( true ),
+			{},
+			[],
+			function(){}
+		];
+
+		for ( var i = 0; i < values.length; i++ ) {
+			expect( badValue( values[i] ) ).to.throw( TypeError );
+		}
+
+		function badValue( value ) {
+			return function() {
+				divide( [1,2,3], 10, {
+					'copy': value
+				});
+			};
+		}
+	});
+
+	it( 'should throw an error if provided an accessor option which is not a function', function test() {
+		var values = [
+			'5',
+			5,
+			null,
+			undefined,
+			NaN,
+			{},
+			[]
+		];
+
+		for ( var i = 0; i < values.length; i++ ) {
+			expect( badValue( values[i] ) ).to.throw( TypeError );
+		}
+
+		function badValue( value ) {
+			return function() {
+				divide( [1,2,3], 10, {
+					'accessor': value
+				});
+			};
+		}
+	});
+
+	it( 'should throw an error if provided an array as a second argument which is not of equal length to the input array', function test() {
 		expect( foo ).to.throw( Error );
 		function foo() {
 			divide( [1,2], [1,2,3] );
 		}
 	});
 
+	it( 'should not mutate an input array by default', function test() {
+		var data, actual;
+
+		data = [ 4, 5, 3, 6, 8 ];
+		actual = divide( data, 4 );
+
+		assert.ok( data !== actual );
+	});
+
 	it( 'should perform element-wise division', function test() {
-		var data, expected;
+		var data, expected, actual;
 
+		// Scalar:
 		data = [ 5, 2, 4, 1, 2 ];
-		expected = [ 5/4, 0.5, 1, 0.25, 0.5 ];
 
-		divide( data, 4 );
-		assert.deepEqual( data, expected );
+		actual = divide( data, 4 );
+		expected = [ 1.25, 0.5, 1, 0.25, 0.5 ];
 
+		assert.deepEqual( actual, expected );
+
+		// Array of numeric values:
 		data = [ 5, 2, 4, 1, 2 ];
+
+		actual = divide( data, [ 5, 2, 4, 1, 1 ] );
 		expected = [ 1, 1, 1, 1, 2 ];
 
-		divide( data, [5,2,4,1,1] );
-		assert.deepEqual( data, expected );
+		assert.deepEqual( actual, expected );
+	});
+
+	it( 'should mutate an input array if the `copy` option is `false`', function test() {
+		var data, expected, actual;
+
+		data = [ 4, 5, 3, 6, 8 ];
+
+		actual = divide( data, 4, {
+			'copy': false
+		});
+		expected = [ 1, 1.25, 0.75, 1.5, 2 ];
+
+		assert.ok( data === actual );
+		assert.deepEqual( actual, expected );
+	});
+
+	it( 'should perform element-wise division using an accessor', function test() {
+		var data, expected, actual;
+
+		data = [
+			{'x':5},
+			{'x':2},
+			{'x':4},
+			{'x':1},
+			{'x':2}
+		];
+		expected = [ 1.25, 0.5, 1, 0.25, 0.5 ];
+
+		actual = divide( data, 4, {
+			'accessor': getValue
+		});
+		assert.deepEqual( actual, expected );
+
+		function getValue( d ) {
+			return d.x;
+		}
+	});
+
+	it( 'should perform element-wise division when provided an array and using an accessor', function test() {
+		var data, arr, expected, actual;
+
+		data = [
+			{'x':5},
+			{'x':2},
+			{'x':4},
+			{'x':1},
+			{'x':2}
+		];
+
+		// One array accessed...
+		arr = [ 5, 2, 4, 1, 1 ];
+
+		actual = divide( data, arr, {
+			'accessor': getValue1
+		});
+		expected = [ 1, 1, 1, 1, 2 ];
+
+		assert.deepEqual( actual, expected );
+
+		// Both arrays are accessed...
+		arr = [
+			[0,5],
+			[1,2],
+			[2,4],
+			[3,1],
+			[4,1]
+		];
+
+		actual = divide( data, arr, {
+			'accessor': getValue2
+		});
+		expected = [ 1, 1, 1, 1, 2 ];
+
+		assert.deepEqual( actual, expected );
+
+		function getValue1( d ) {
+			return d.x;
+		}
+		function getValue2( d, i, j ) {
+			if ( j === 0 ) {
+				return d.x;
+			}
+			return d[ 1 ];
+		}
 	});
 
 });
